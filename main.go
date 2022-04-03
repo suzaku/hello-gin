b00cf10b209da3f08bae5fc8a5c74f6e2158d410
satoru <satorulogic@gmail.com>
2022-04-03 15:17:19 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	if err := r.Run(":"+port); err != nil {
+		log.Panicf("error: %s", err)
+	}
+}
